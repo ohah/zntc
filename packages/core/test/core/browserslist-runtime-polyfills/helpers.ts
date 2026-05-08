f800c3d9c0a5e3a4cf94adf80ded9f43aa09c3be
ohah <bookyoon173@gmail.com>
2026-05-08 22:37:13 +0900

    test(core): split browserslist runtime polyfill tests

---
+++ packages/core/test/core/browserslist-runtime-polyfills/helpers.ts
@@ -0,0 +1,15 @@
+export {
+  describe,
+  test,
+  expect,
+  transpile,
+  build,
+  buildSync,
+  mkdtempSync,
+  mkdirSync,
+  writeFileSync,
+  rmSync,
+  join,
+  tmpdir,
+} from '../helpers';
+export type { ZntcPlugin } from '../helpers';
