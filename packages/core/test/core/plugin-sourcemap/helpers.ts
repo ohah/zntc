6847bc219d0671206b2ec0f748a587e9a2071950
ohah <bookyoon173@gmail.com>
2026-05-09 01:28:43 +0900

    test(core): split plugin sourcemap tests

---
+++ packages/core/test/core/plugin-sourcemap/helpers.ts
@@ -0,0 +1,19 @@
+export {
+  describe,
+  test,
+  expect,
+  build,
+  watch,
+  mkdtempSync,
+  writeFileSync,
+  readFileSync,
+  rmSync,
+  join,
+  tmpdir,
+  expectPluginDiagnostic,
+  lineOffsetMappings,
+  parseBundleMap,
+  expectMarkerMappedToSourceLine,
+} from '../helpers';
+
+export type { ZntcPlugin } from '../helpers';
